4a8fdb4ba6e479f963de147177bdaef0caafadf2
Y_Shinmen <109737068+shin-yus@users.noreply.github.com>
2022-10-12 15:56:40 +0900

    Add files via upload

---
+++ calender/inc/footer.php
@@ -0,0 +1,8 @@
+    </br>
+    <footer class="footer">
+        <div class="footer-inner">
+            <p>&copy; 2022 YUSUKE SHINMEN</p>
+        <div>
+    </footer>
+</body>
+</html>
---
+++ calender/inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <link rel="icon" href="favicon.ico" id="favicon">
+    <link rel="stylesheet" type="text/css" href="http://yshinmen.php.xdomain.jp/calender/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="http://yshinmen.php.xdomain.jp/calender/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
+    <title>calender</title>
+</head>
+<body>
+<header class="header">
+  <div class="header-inner">
+    <h2>WORKS</h2>
+  </div>
+</header>
